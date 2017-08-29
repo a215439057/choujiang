928740ca7d52fbf161dfefa379c1f783db4f9c55
a215439057 <a215439057@qq.com>
2017-08-29 11:30:39 +0800

    wrote a readme file

--- js/a.js
+++ js/a.js
@@ -1,4 +1,5 @@
 	function ord(){
+		document.getElementById('button1').disabled=true;
 		var order =document.getElementById('order');
 		var mali =order.getElementsByTagName('li'); 
 		var is =0;
@@ -55,8 +56,60 @@
 		mali[is-1].style.backgroundColor ="#3a8fb7";
 		if (mali[is-1].innerHTML=="谢谢惠顾") {
 			tex.innerHTML=mali[is-1].innerHTML;
+			document.getElementById('button1').disabled=false;	
 		}
 		else{
 			tex.innerHTML="恭喜获得"+mali[is-1].innerHTML;
+			document.getElementById('button1').disabled=false;	
 		}	
 	}		
+//第二个//
+	function dis(){
+		document.getElementById('button2').disabled=true;
+		var diso =document.getElementById('disorder');
+		var doso =diso.getElementsByTagName('li'); 
+		var chbn =doso.length;	
+		for (var i = 0; i <chbn; i++) {
+				doso[i].style.backgroundColor ="#eee";
+			}
+		var iq=0;
+		var num=0;
+		var nem=100;
+		or(num,iq,nem);
+	}
+function or(num,iq,nem){
+		var diso =document.getElementById('disorder');
+		var doso =diso.getElementsByTagName('li'); 	
+		var tec =document.getElementById('tec');
+		if (iq!=20) {
+			var num = (Math.ceil(Math.random()*doso.length))-1;	
+			if (nem==num) {
+				or(num,iq,nem);
+					}
+			else{
+				doso[num].style.backgroundColor ="#3a8fb7";
+				nem =num;
+				tome =setTimeout(function(){
+					our(num,iq,nem);	
+				},200);
+			}		
+	
+		}
+		else{
+			doso[num].style.backgroundColor ="#3a8fb7";
+			if (doso[num].innerHTML=="谢谢惠顾") {
+				tec.innerHTML=doso[num].innerHTML;
+			}
+			else{
+				tec.innerHTML="恭喜获得"+doso[num].innerHTML;
+			}
+		document.getElementById('button2').disabled=false;	
+		}
+}	
+function our(num,iq,nem){
+	var diso =document.getElementById('disorder');
+	var doso =diso.getElementsByTagName('li'); 	
+	iq++;
+	doso[num].style.backgroundColor ="#eee";
+	or(num,iq,nem);
+}
